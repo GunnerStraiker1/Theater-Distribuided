0aec75d5fa61ea194f598be1be6ba06f978c668f
Armando Carvajal <armandoecr_96@hotmail.com>
2018-05-28 11:38:12 -0500

    Seleccion y deseleccion de progarama local, ajustes visuales

--- src/cliente/AsientosCtrl.java
+++ src/cliente/AsientosCtrl.java
@@ -5,186 +5,92 @@
  */
 package cliente;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import model.patitoAPI;
 //
 ///**
 // *
 // * @author Victor Perera
 // */
-public class AsientosCtrl implements ActionListener{
-    
+
+public class AsientosCtrl implements ActionListener {
+
     private AsientosFrame view;
     private patitoAPI rp;
+    private int asientosSeleccionados;
     private List<JButton> buttons = new ArrayList<>();
-
 
     public AsientosCtrl(AsientosFrame view, patitoAPI rp) {
         this.view = view;
         this.rp = rp;
         this.view.setLocationRelativeTo(null);
         this.view.setVisible(true);
+        this.asientosSeleccionados = 0;
         crearPanelAsientos();
+        this.view.buy.addActionListener(this);
     }
-    
-    
-    
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this.view.buy) {
+            for (int i = 0; i < buttons.size(); i++) {
+                if(buttons.get(i).getBackground().equals(Color.BLACK)){
+                    System.out.println(buttons.get(i).getBackground().toString());
+                }
+            }
         }
-    
-//    private AsientosFrame asientosView;
-    
-//    private DAOAsientos daoAsiento;
-//    private List<Asiento> listAsientos;
-//    private List<JButton> buttons = new ArrayList<>();
-//    private int noAsientosSelect;
-//    private List<Asiento> asientosSelect = new ArrayList<>();
-//
-//    public MainCtrl(AsientosFrame asientosView, DAOAsiento daoAsiento) {
-//        this.asientosView = asientosView;
-//        this.daoAsiento = daoAsiento;
-//        this.asientosView.setLocationRelativeTo(null);
-//        this.asientosView.buy.addActionListener(this);
-//        listAsientos = daoAsiento.consultarAsientosGral();
-//        this.noAsientosSelect=0;
-//        crearPanelAsientos(obra);
-//        this.asientosView.setVisible(true);        
-//    }
-//
-//    public AsientosFrame getAsientosView() {
-//        return asientosView;
-//    }
-//
-//    public void setAsientosView(AsientosFrame asientosView) {
-//        this.asientosView = asientosView;
-//    }
-//
-//    public DAOAsiento getDaoAsiento() {
-//        return daoAsiento;
-//    }
-//
-//    public void setDaoAsiento(DAOAsiento daoAsiento) {
-//        this.daoAsiento = daoAsiento;
-//    }
-//
-//    public List<Asiento> getListAsientos() {
-//        return listAsientos;
-//    }
-//
-//    public void setListAsientos(List<Asiento> listAsientos) {
-//        this.listAsientos = listAsientos;
-//    }
-//
-//    public List<JButton> getButtons() {
-//        return buttons;
-//    }
-//
-//    public void setButtons(List<JButton> buttons) {
-//        this.buttons = buttons;
-//    }
-//
-//    public int getNoAsientosSelect() {
-//        return noAsientosSelect;
-//    }
-//
-//    public void setNoAsientosSelect(int noAsientosSelect) {
-//        this.noAsientosSelect = noAsientosSelect;
-//    }
-//        
+    }
+     
     private void crearPanelAsientos() {
         for (int i = 0; i < 100; i++) {
 
             JButton lab = new JButton();
-            lab.setBackground(new java.awt.Color(0,205,0));
-            this.view.panel.add(lab);        
+            lab.setBackground(Color.GREEN);
+            this.view.panel.add(lab);
 
             lab.addActionListener((ActionEvent e) -> {
-//                        if (!lab.getBackground().equals(Color.BLACK)) {
-//                            if (getNoAsientosSelect() < Integer.parseInt(asientosView.noBoletos.getText())) {
-//                            seleccionarAsiento(lab, obra);
-//                            setNoAsientosSelect(getNoAsientosSelect() + 1);
-//                        } else {
-//                                JOptionPane.showMessageDialog(asientosView, "Has alcanzado tu limite de asientos");
-//                            
-//                        }
-//                    }
-//                    else{
-//                            deseleccionarAsiento(lab);
-//                            setNoAsientosSelect(getNoAsientosSelect() - 1);
-//                        
-//                    }
+
+                if (!lab.getBackground().equals(Color.GREEN)) {
+                    if (lab.getBackground().equals(Color.BLACK)) {
+                        this.asientosSeleccionados--;
+                        lab.setBackground(Color.GREEN);
+                    }
+
+                    if (lab.getBackground().equals(Color.GRAY)) {
+                        JOptionPane.showMessageDialog(view, "Asientos seleccionados anteriormente", "Advertencia", JOptionPane.INFORMATION_MESSAGE);
+                    }
+
+                    if (lab.getBackground().equals(Color.BLUE)) {
+                        JOptionPane.showMessageDialog(view, "Siendo reservado", "Advertencia", JOptionPane.WARNING_MESSAGE);
+                    }
+
+                    if (lab.getBackground().equals(Color.RED)) {
+                        JOptionPane.showMessageDialog(view, "Ocupado", "Advertencia", JOptionPane.INFORMATION_MESSAGE);
+                    }
+
+                } else {
+                    if (this.asientosSeleccionados < 5) {
+                        this.asientosSeleccionados++;
+                        lab.setBackground(Color.BLACK);
+                    } else {
+                        JOptionPane.showMessageDialog(view, "Limite de asientos alcanzado para está sesión", "Limite", JOptionPane.INFORMATION_MESSAGE);
+
+                    }
+
+                }
             });
-            buttons.add(lab);  
+            buttons.add(lab);
             this.view.panel.updateUI();
         }
     }
 }
-//    
-//    private void seleccionarAsiento(JButton boton, Obra obra){
-//            Color bg = boton.getBackground();
-//            String palco = null;
-//            double porcentaje=0;
-//            if (bg.equals(Color.BLUE)) {
-//                palco = "Plata";
-//                porcentaje=.75;
-//            }
-//            else if(bg.equals(Color.YELLOW)){
-//                palco = "Oro";
-//                porcentaje=.9;
-//            }
-//            else if(bg.equals(Color.GREEN)){
-//                palco = "Diamante";
-//                porcentaje=1;
-//            }
-//            else if(bg.equals(Color.LIGHT_GRAY)){
-//                palco = "Lata";
-//                porcentaje=.5;
-//            }
-//            else if(bg.equals(Color.ORANGE)){
-//                palco = "Cobre";
-//                porcentaje=.6;
-//            }
-//            double precio = daoAsiento.consultarPrecio(obra);
-//        
-//            Asiento asientoSelect = new Asiento(boton.getText(),palco,(precio*porcentaje));
-////            System.out.println(asi.getNombre());
-//            asientosSelect.add(asientoSelect);
-//            
-//            boton.setBackground(Color.BLACK);
-//            boton.setForeground(Color.BLACK);
-//    }
-//    
-//    public void deseleccionarAsiento(JButton sit){
-//        for (int j = 0; j < asientosSelect.size(); j++) {
-//            if(asientosSelect.get(j).getNombre().equals(sit.getText())){
-//                if(asientosSelect.get(j).getArea().equalsIgnoreCase("Plata")){
-//                   sit.setBackground(Color.BLUE); 
-//                }
-//                else if(asientosSelect.get(j).getArea().equalsIgnoreCase("Oro")){
-//                   sit.setBackground(Color.YELLOW); 
-//                }
-//                else if(asientosSelect.get(j).getArea().equalsIgnoreCase("Diamante")){
-//                   sit.setBackground(Color.GREEN); 
-//                }
-//                else if(asientosSelect.get(j).getArea().equalsIgnoreCase("Lata")){
-//                   sit.setBackground(Color.LIGHT_GRAY); 
-//                }
-//                else if(asientosSelect.get(j).getArea().equalsIgnoreCase("Cobre")){
-//                   sit.setBackground(Color.ORANGE); 
-//                }
-//                sit.setEnabled(true);
-//                asientosSelect.remove(j);
-//                //i--;
-//            }
-//        }
-//    }
-//    
+
 //    @Override
 //    public void actionPerformed(ActionEvent e) {
 //        if (e.getSource() == this.asientosView.buy) {
--- src/cliente/AsientosFrame.java
+++ src/cliente/AsientosFrame.java
@@ -6,8 +6,7 @@
  */
 package cliente;
 
-import java.util.ArrayList;
-import javax.swing.JButton;
+import java.awt.Color;
 import javax.swing.JOptionPane;
 
 /**
@@ -22,7 +21,12 @@ public class AsientosFrame extends javax.swing.JFrame {
     public AsientosFrame() {
        
         initComponents();
-        panel.setVisible(true);        
+        panel.setVisible(true);
+        jPDisponible.setBackground(Color.GREEN);
+        jPOcupados.setBackground(Color.RED);
+        jPSiendoReservado.setBackground(Color.BLUE);
+        jPMisLugares.setBackground(Color.GRAY);
+        jPReservacionActual.setBackground(Color.BLACK);
     }
     /**
      * This method is called from within the constructor to initialize the form.
@@ -39,15 +43,15 @@ public class AsientosFrame extends javax.swing.JFrame {
         buy = new javax.swing.JButton();
         noBoletos = new javax.swing.JTextField();
         startSelection = new javax.swing.JButton();
-        jPanel1 = new javax.swing.JPanel();
+        jPOcupados = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jPanel2 = new javax.swing.JPanel();
+        jPMisLugares = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
-        jPanel3 = new javax.swing.JPanel();
+        jPSiendoReservado = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
-        jPanel4 = new javax.swing.JPanel();
+        jPDisponible = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
-        jPanel5 = new javax.swing.JPanel();
+        jPReservacionActual = new javax.swing.JPanel();
         jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -79,77 +83,66 @@ public class AsientosFrame extends javax.swing.JFrame {
             }
         });
 
-        jPanel1.setBackground(new java.awt.Color(255, 0, 0));
-        jPanel1.setForeground(new java.awt.Color(255, 0, 0));
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout jPOcupadosLayout = new javax.swing.GroupLayout(jPOcupados);
+        jPOcupados.setLayout(jPOcupadosLayout);
+        jPOcupadosLayout.setHorizontalGroup(
+            jPOcupadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 33, Short.MAX_VALUE)
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        jPOcupadosLayout.setVerticalGroup(
+            jPOcupadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 31, Short.MAX_VALUE)
         );
 
         jLabel1.setText("Ocupados");
 
-        jPanel2.setBackground(new java.awt.Color(204, 204, 204));
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout jPMisLugaresLayout = new javax.swing.GroupLayout(jPMisLugares);
+        jPMisLugares.setLayout(jPMisLugaresLayout);
+        jPMisLugaresLayout.setHorizontalGroup(
+            jPMisLugaresLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 33, Short.MAX_VALUE)
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        jPMisLugaresLayout.setVerticalGroup(
+            jPMisLugaresLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 33, Short.MAX_VALUE)
         );
 
         jLabel2.setText("Mis Lugares");
 
-        jPanel3.setBackground(new java.awt.Color(0, 0, 255));
-
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout jPSiendoReservadoLayout = new javax.swing.GroupLayout(jPSiendoReservado);
+        jPSiendoReservado.setLayout(jPSiendoReservadoLayout);
+        jPSiendoReservadoLayout.setHorizontalGroup(
+            jPSiendoReservadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 33, Short.MAX_VALUE)
         );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        jPSiendoReservadoLayout.setVerticalGroup(
+            jPSiendoReservadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 32, Short.MAX_VALUE)
         );
 
         jLabel3.setText("Siendo reservado");
 
-        jPanel4.setBackground(new java.awt.Color(0, 204, 0));
-
-        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
-        jPanel4.setLayout(jPanel4Layout);
-        jPanel4Layout.setHorizontalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout jPDisponibleLayout = new javax.swing.GroupLayout(jPDisponible);
+        jPDisponible.setLayout(jPDisponibleLayout);
+        jPDisponibleLayout.setHorizontalGroup(
+            jPDisponibleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 33, Short.MAX_VALUE)
         );
-        jPanel4Layout.setVerticalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        jPDisponibleLayout.setVerticalGroup(
+            jPDisponibleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 34, Short.MAX_VALUE)
         );
 
         jLabel4.setText("Disponible");
 
-        jPanel5.setBackground(new java.awt.Color(0, 0, 0));
-
-        javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
-        jPanel5.setLayout(jPanel5Layout);
-        jPanel5Layout.setHorizontalGroup(
-            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout jPReservacionActualLayout = new javax.swing.GroupLayout(jPReservacionActual);
+        jPReservacionActual.setLayout(jPReservacionActualLayout);
+        jPReservacionActualLayout.setHorizontalGroup(
+            jPReservacionActualLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 33, Short.MAX_VALUE)
         );
-        jPanel5Layout.setVerticalGroup(
-            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        jPReservacionActualLayout.setVerticalGroup(
+            jPReservacionActualLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 34, Short.MAX_VALUE)
         );
 
@@ -167,24 +160,24 @@ public class AsientosFrame extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jPOcupados, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jLabel1))
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jPMisLugares, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jLabel2))
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jPSiendoReservado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jLabel3))
                             .addComponent(buy)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jPDisponible, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jLabel4))
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jPanel5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jPReservacionActual, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jLabel5))))
                     .addGroup(layout.createSequentialGroup()
@@ -208,23 +201,23 @@ public class AsientosFrame extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jPOcupados, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel1))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jPMisLugares, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel2))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jPSiendoReservado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel3))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jPDisponible, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel4))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jPanel5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jPReservacionActual, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel5))
                         .addGap(279, 279, 279)
                         .addComponent(buy))
@@ -236,8 +229,6 @@ public class AsientosFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void buyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buyActionPerformed
-       // ca.comprarAsientos();
-        
        
        
     }//GEN-LAST:event_buyActionPerformed
@@ -310,11 +301,11 @@ public class AsientosFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JPanel jPanel4;
-    private javax.swing.JPanel jPanel5;
+    private javax.swing.JPanel jPDisponible;
+    private javax.swing.JPanel jPMisLugares;
+    private javax.swing.JPanel jPOcupados;
+    private javax.swing.JPanel jPReservacionActual;
+    private javax.swing.JPanel jPSiendoReservado;
     private javax.swing.JScrollPane jScrollPane1;
     public javax.swing.JTextField noBoletos;
     public javax.swing.JPanel panel;
